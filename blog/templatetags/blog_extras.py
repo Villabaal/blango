5ab2865f875ec79e33193a848af03f1d0f8d120c
Ing. Villalobos <alejandro.villalobos.barajas@gmail.com>
2024-04-03 01:31:59 +0000

    Finish custom template tags

--- blog/templatetags/blog_extras.py
+++ blog/templatetags/blog_extras.py
@@ -1,11 +1,13 @@
 from django.contrib.auth.models import User
 from django.template import Library
 from django.utils.html import format_html
+from typing import Union
+from blog.models import Post
 
 register = Library()
 
 @register.filter
-def author_details(author: User, current_user:User|None = None) -> str:
+def author_details(author: User, current_user:Union[User,None] = None) -> str:
     if not isinstance(author, User):
         # return empty string as safe default
         return ""
@@ -24,3 +26,25 @@ def author_details(author: User, current_user:User|None = None) -> str:
         suffix = ""
 
     return format_html('{}{}{}', prefix, name, suffix)
+
+
+@register.simple_tag
+def row(extra_classes=''):
+  return format_html('<div class="row {}">', extra_classes)
+
+@register.simple_tag
+def endrow():
+  return format_html('</div>')
+
+@register.simple_tag
+def col(extra_classes=""):
+    return format_html('<div class="col {}">', extra_classes)
+
+@register.simple_tag
+def endcol():
+    return format_html("</div>")
+
+@register.inclusion_tag("blog/post-list.html")
+def recent_posts(post):
+    posts = Post.objects.exclude(pk=post.pk)[:5]
+    return {"title": "Recent Posts", "posts": posts}
