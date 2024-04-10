21a0e97f5ebfa38ee629f75285cd12c81364b053
Ing. Villalobos <alejandro.villalobos.barajas@gmail.com>
2024-04-10 06:45:35 +0000

    Finish logging

--- blango/settings.py
+++ blango/settings.py
@@ -15,6 +15,27 @@ import os
 import dj_database_url
 
 class Dev(Configuration):
+  LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "formatters": {
+        "verbose": {
+            "format": "{levelname} {asctime} {module} {process:d} {thread:d} {message}",
+            "style": "{",
+        },
+    },
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+            "stream": "ext://sys.stdout",
+            "formatter": "verbose",
+        },
+    },
+    "root": {
+        "handlers": ["console"],
+        "level": "DEBUG",
+    },
+  }
   # Build paths inside the project like this: BASE_DIR / 'subdir'.
   BASE_DIR = Path(__file__).resolve().parent.parent
   
