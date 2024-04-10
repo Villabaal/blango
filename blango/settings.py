b2b5df1b1f6d3553068c567590ad501d627b9102
Ing. Villalobos <alejandro.villalobos.barajas@gmail.com>
2024-04-10 14:40:12 +0000

    Finish security ad passwords

--- blango/settings.py
+++ blango/settings.py
@@ -15,6 +15,10 @@ import os
 import dj_database_url
 
 class Dev(Configuration):
+
+  # Build paths inside the project like this: BASE_DIR / 'subdir'.
+  BASE_DIR = Path(__file__).resolve().parent.parent
+  
   LOGGING = {
     "version": 1,
     "disable_existing_loggers": False,
@@ -36,10 +40,7 @@ class Dev(Configuration):
         "level": "DEBUG",
     },
   }
-  # Build paths inside the project like this: BASE_DIR / 'subdir'.
-  BASE_DIR = Path(__file__).resolve().parent.parent
-  
-  
+
   # Quick-start development settings - unsuitable for production
   # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
   
