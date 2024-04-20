b5ada0d8ec3fd5c8a9d88835ffaa8a74d464380d
Ing. Villalobos <alejandro.villalobos.barajas@gmail.com>
2024-04-20 19:35:15 +0000

    Finish django-versatileimagefield

--- blango/settings.py
+++ blango/settings.py
@@ -96,6 +96,10 @@ class Dev(Configuration):
       "ACCESS_TOKEN_LIFETIME": timedelta(days=1),
       "REFRESH_TOKEN_LIFETIME": timedelta(days=7),
   }
+
+  MEDIA_ROOT = BASE_DIR / "media"
+  MEDIA_URL = "/media/"
+
   # Quick-start development settings - unsuitable for production
   # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
   
@@ -138,6 +142,7 @@ class Dev(Configuration):
       "rest_framework.authtoken",
       "drf_yasg",
       "django_filters",
+      "versatileimagefield",
   ]
   
   MIDDLEWARE = [
